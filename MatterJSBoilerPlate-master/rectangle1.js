29e2ad017399a90a68b9cb2d3b1179c4eb28b7ce
vivan-123 <69099674+vivan-123@users.noreply.github.com>
2020-10-25 20:14:54 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/rectangle1.js
@@ -0,0 +1,25 @@
+/*class Rectangle1
+{
+    constructor(x,y,w,h)
+    {
+        var options =
+        {
+            isStatic : true
+        }
+
+        this.body = Bodies.Rectangle(x,y,w,h,options);
+        this.body.position.x = x;
+        this.body.position.y = y;
+        this.body.width = w;
+        this.body.height = h;
+        World.add(world, this.body);
+    }
+
+    display()
+    {
+        fill("red");
+        Rectangle(this.body.position.x,this.body.position.y,this.body.width,this.body.height);
+        
+    }
+}
+*/
